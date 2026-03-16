e0d0b6e9b0c901ee48811b3f0242cb9be1e674c7
aartchik <artyr-nasredin@yandex.ru>
2026-03-16 03:04:08 +0300

    fixed jobs

--- README.md
+++ README.md
@@ -30,19 +30,20 @@ golangci-lint custom
 
 При запуске линтера внутри директории linter должно вывести:
 ```bash
-        slog.Error("ошибка подключения") // want "log message should contain only English letters"
-                   ^
-test/checker.go:11:12: logrecords: log message contains special symbols or emoji (mylinter)
-        slog.Info("server started 🚀") // want "log message contains special symbols or emoji"
-                  ^
-test/checker.go:12:12: logrecords: log message contains special symbols or emoji (mylinter)
-        slog.Warn("warning: something went wrong") // want "log message contains special symbols or emoji"
-                  ^
-test/checker.go:13:13: logrecords: log message contains special symbols or emoji (mylinter)
-        slog.Error("connection failed...") // want "log message contains special symbols or emoji"
-                   ^
-5 issues:
-* mylinter: 5
+        test/checker.go:8:20: logrecords: log message should contain only English letters (mylinter)
+                slog.Error("ошибка подключения") 
+                        ^
+        test/checker.go:9:19: logrecords: log message contains special symbols or emoji (mylinter)
+                slog.Info("server started 🚀") 
+                        ^
+        test/checker.go:10:19: logrecords: log message contains special symbols or emoji (mylinter)
+                slog.Warn("warning: something went wrong")
+                        ^
+        test/checker.go:11:20: logrecords: log message contains special symbols or emoji (mylinter)
+                slog.Error("connection failed...") 
+                        ^
+4 issues:
+* mylinter: 4
 ```
 
 Линтер поддерживает автоматическое исправление некоторых ошибок:
@@ -53,14 +54,11 @@ test/checker.go:13:13: logrecords: log message contains special symbols or emoji
 
 При запуске в той же директории:
 ```bash
-test/checker.go:8:12: logrecords: log message should contain only English letters (mylinter)
-        slog.Info("запуск сервера") // want "log message should contain only English letters"
-                  ^
-test/checker.go:9:13: logrecords: log message should contain only English letters (mylinter)
-        slog.Error("ошибка подключения") // want "log message should contain only English letters"
-                   ^
-2 issues:
-* mylinter: 2
+        test/checker.go:8:20: logrecords: log message should contain only English letters (mylinter)
+                slog.Error("ошибка подключения") 
+                           ^
+1 issues:
+* mylinter: 1
 ```
 
 Тесты
--- test/checker.go
+++ test/checker.go
@@ -1,77 +1,12 @@
 package test
 
 import (
-	"context"
 	"log/slog"
-	"os"
 )
 
-
 func main() {
-
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-
-	slog.Info("запуск сервера") // want "log message should contain only English letters"
-	slog.Error("ошибка подключения") // want "log message should contain only English letters"
-
-	logger.Info("запуск сервера") // want "log message should contain only English letters"
-	logger.Error("ошибка подключения") // want "log message should contain only English letters"
-
-	slog.Info("server started 🚀") // want "log message contains special symbols or emoji"
-	slog.Warn("warning: something went wrong") // want "log message contains special symbols or emoji"
-	slog.Error("connection failed...") // want "log message contains special symbols or emoji"
-
-	logger.Info("server started 🚀") // want "log message contains special symbols or emoji"
-	logger.Warn("warning: something went wrong") // want "log message contains special symbols or emoji"
-	logger.Error("connection failed...") // want "log message contains special symbols or emoji"
-
-	slog.WarnContext(context.Background(), "Warning context lower first letter") // want "log message should start with lowercase"
-	logger.WarnContext(context.Background(), "Warning context lower first letter") // want "log message should start with lowercase"
-
-	
-	slog.Info("message", "Field1", "value1") // want "log field key should start with lowercase"
-	slog.Info("message", "field!!!", "value1") // want "log field key contains special symbols or emoji"
-	slog.Info("message", "ключ", "value1") // want "log field key should contain only English letters"
-
-	logger.Info("message", "Field1", "value1") // want "log field key should start with lowercase"
-	logger.Info("message", "field!!!", "value1") // want "log field key contains special symbols or emoji"
-	logger.Info("message", "ключ", "value1") // want "log field key should contain only English letters"
-
-	
-	slog.Info("message", slog.String("Field1", "value1")) // want "log field key should start with lowercase"
-	slog.Info("message", slog.String("field!!!", "value1")) // want "log field key contains special symbols or emoji"
-	slog.Info("message", slog.String("ключ", "value1")) // want "log field key should contain only English letters"
-
-	logger.Info("message", slog.String("Field1", "value1")) // want "log field key should start with lowercase"
-	logger.Info("message", slog.String("field!!!", "value1")) // want "log field key contains special symbols or emoji"
-	logger.Info("message", slog.String("ключ", "value1")) // want "log field key should contain only English letters"
-
-
-	password := ""
-	net := ""
-	Password := ""
-	slog.Info("message", slog.String("test", Password))
-	slog.Info("message", "test", password) // want "log field key contains potential sensitive word"
-	slog.Info("message", "test", net) 
-
-	logger.Info("message", slog.String("password", net)) // want "log field key contains potential sensitive word"
-	logger.Info("message", slog.String("test", password)) // want "log field key contains potential sensitive word"
-	logger.Info("message", slog.String("password", net)) // want "log field key contains potential sensitive word"
-	logger.Info("message", "password", password) // want "log field key contains potential sensitive word" "log field key contains potential sensitive word"
-	logger.Info("message", "test", password) // want "log field key contains potential sensitive word" 
-	slog.Info("Upper first letter!!!") // want "log message should start with lowercase" "log message contains special symbols or emoji"
-	logger.Info("Пароль!!!") // want "log message should start with lowercase" "log message should contain only English letters" "log message contains special symbols or emoji"
-
-	slog.InfoContext(context.Background(), "message", "Field1", "value1") // want "log field key should start with lowercase"
-	slog.InfoContext(context.Background(), "message", slog.String("password", password)) // want "log field key contains potential sensitive word" "log field key contains potential sensitive word"
-
-	slog.InfoContext(context.Background(), "password validate", "field1", "value1")
-
-	logger.InfoContext(context.Background(), "message", "Field1", "value1") // want "log field key should start with lowercase"
-	logger.InfoContext(context.Background(), "message", slog.String("password", password)) // want "log field key contains potential sensitive word" "log field key contains potential sensitive word"
-
-	logger.Info("request completed", "field", password) // want "log field key contains potential sensitive word"
-	logger.Info("message", "test", password) // want "log field key contains potential sensitive word"
-	logger.Info("message", "password", net) // want "log field key contains potential sensitive word"
-
+        slog.Error("ошибка подключения") 
+        slog.Info("server started 🚀") 
+        slog.Warn("warning: something went wrong")
+        slog.Error("connection failed...") 
 }
